3574985974bb2f475efcbe73ba49efe7778180b4
macmac-12 <marana@my.cspc.edu.ph>
2026-04-30 01:08:41 +0800

    Changes

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route("/") 
 def home(): 
     return "Hello, CI/CD! I am Macmac! Your Lingkod from BSCS3A"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=2000)
--- test_app.py
+++ test_app.py
@@ -3,4 +3,5 @@ from app import app
 def test_home(): 
     client = app.test_client() 
     response = client.get("/")
-    assert response.status_code == 500
+    assert response.status_code == 200
+    assert response.data == b"Hello, CI/CD! I am Macmac! Your Lingkod from BSCS3A"
